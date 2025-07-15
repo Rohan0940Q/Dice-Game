bcec8f8fce723ca6c2b57fc51dedd855fd3e64c2
Rohan0940Q <127778387+Rohan0940Q@users.noreply.github.com>
2025-07-16 02:01:05 +0530

    Add files via upload
    Dice Game

---
+++ index.js
@@ -0,0 +1,25 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+console.log("Random Number 1:", randomNumber1);
+
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage1;
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var image1 = document.querySelectorAll("img")[0];
+var image2 = document.querySelectorAll("img")[1];
+
+image1.setAttribute("src", randomImageSource);
+image2.setAttribute("src", randomImageSource2);
+
+var heading = document.querySelector("h1");
+
+if (randomNumber1 > randomNumber2) {
+  heading.innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  heading.innerHTML = "Player 2 Wins! 🚩";
+} else {
+  heading.innerHTML = "Draw!";
+}
